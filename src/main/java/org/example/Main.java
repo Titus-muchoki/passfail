cd4b6abfafc048f08a28351c758ece8cb48ea840
kajela <titoyut56@gmail.com>
2022-07-14 11:38:02 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,57 @@
-package org.example;
+package org.example;//package org.example;
+//
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void Score(int score){
+//        System.out.println("enter marks"+score);
+//        if (score >= 50) {
+//            System.out.println("you passed your exam");
+//        } else if (score <= 50) {
+//            System.out.println("you failed the test");
+//        } else {
+//            System.out.println("invalid output");
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println("*******************");
+//        System.out.println(1+"continue");
+//        System.out.println(2 +" Exit");
+//        System.out.println("");
+//            Scanner myObj = new Scanner(System.in);
+//        int userInput = myObj.nextInt();
+//        if (userInput == 1){
+//            System.out.println("");
+//            System.out.println("input the length of triangle");
+//            System.out.println("***************");
+//            System.out.println("");
+//            Score(marks){
+//                System.out.println();
+//            }
+//        }
+//
+//
+//    }
 
 import java.util.Scanner;
 
-public class Main {
-    public static void Score(int score){
-        System.out.println("enter marks"+score);
-        if (score >= 50) {
-            System.out.println("you passed your exam");
-        } else if (score <= 50) {
-            System.out.println("you failed the test");
-        } else {
-            System.out.println("invalid output");
-        }
-    }
-
+ class PassFail {
     public static void main(String[] args) {
-        System.out.println("*******************");
-        System.out.println(1+"continue");
-        System.out.println(2 +" Exit");
-        System.out.println("");
-            Scanner myObj = new Scanner(System.in);
-        int userInput = myObj.nextInt();
-        if (userInput == 1){
-            System.out.println("");
-            System.out.println("input the length of triangle");
-            System.out.println("***************");
-            System.out.println("");
-            Score(marks){
-                System.out.println();
-            }
-        }
+        int score;
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter score: ");
+        score = reader.nextInt();
 
+        if (score>=50)
+        {
+            System.out.println("Pass!");
+        }
+        else if (score<=50){
+            System.out.println("Fail!");
+        }else
+            System.out.println("invalid input");
 
     }
+
 }
